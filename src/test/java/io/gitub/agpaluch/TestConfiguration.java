c35312a0bd7fc9adf5d258dbc48b0ece17060c06
Agnieszka <agnieszka.paluch90@gmail.com>
2020-04-28 17:32:44 +0200

    Some E2E and integartion tests

--- src/test/java/io/gitub/agpaluch/TestConfiguration.java
+++ src/test/java/io/gitub/agpaluch/TestConfiguration.java
@@ -1,6 +1,7 @@
 package io.gitub.agpaluch;
 
 import io.gitub.agpaluch.model.Task;
+import io.gitub.agpaluch.model.TaskGroup;
 import io.gitub.agpaluch.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +9,26 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
+import java.lang.reflect.Field;
 import java.util.*;
 
 @Configuration
 class TestConfiguration {
 
     @Bean
+    @Primary
+    @Profile("!integration")
+    DataSource e2eTestDataSource(){
+        DriverManagerDataSource result = new DriverManagerDataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
+        result.setDriverClassName("org.h2.Driver");
+        return result;
+    }
+
+    @Bean
+    @Primary
     @Profile("integration")
     TaskRepository testRepo() {
         return new TaskRepository() {
@@ -42,7 +56,16 @@ class TestConfiguration {
 
             @Override
             public Task save(final Task entity) {
-                return tasks.put(tasks.size() + 1, entity);
+                int key = tasks.size() + 1;
+                try {
+                    Field field = field = Task.class.getDeclaredField("id");
+                    field.setAccessible(true);
+                    field.set(entity, key);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+                tasks.put(key, entity);
+                return tasks.get(key);
             }
 
             @Override
---
+++ src/test/java/io/gitub/agpaluch/controller/TaskControllerIntegrationTest.java
@@ -0,0 +1,44 @@
+package io.gitub.agpaluch.controller;
+
+import io.gitub.agpaluch.model.Task;
+import io.gitub.agpaluch.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+class TaskControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        //given
+        int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
+
+        //when + then
+        mockMvc.perform(get("/tasks/" + id))
+                .andExpect(status().is2xxSuccessful());
+    }
+}
+
+
+
+
